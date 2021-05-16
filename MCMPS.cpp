eb50123cf4d2221b10c47605e6a093f57fc1e907
smreyes <smreyes@smreyes-6538.pods.smreyes.svc.cluster.local>
2021-05-16 12:30:53 -0700

    3

--- MCMPS.cpp
+++ MCMPS.cpp
@@ -11,8 +11,8 @@ int MCMPS() {
 	register int x;
 	register int y;
 	register int z;
-	x = 1000;
-	y = 100000;
+	x = 10000;
+	y = 10000;
 	z = 10000;
 	A = (double *)malloc(sizeof(double)*x*y);
 	B = (double *)malloc(sizeof(double)*x*z);
