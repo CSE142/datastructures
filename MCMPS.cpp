79302a11e2771178864975c0378aae08ea9c0631
smreyes <smreyes@smreyes-12163.pods.smreyes.svc.cluster.local>
2021-05-16 04:52:14 -0700

    d

--- MCMPS.cpp
+++ MCMPS.cpp
@@ -2,8 +2,18 @@
 #include <archlab.h>
 
 int MCMPS() {
-	uint64_t seed = 1;
-	long int sum = 1;
+	std::vector<uint32_t> v;
+	//uint64_t seed = 1;
+	uint64_t sum = 1;
+	uint32_t *restrict velt = &v[0];
+	uint32_t *restrict vend = velt + v.size();
+	
+	while(velt < vend) {
+		sum += *velt;
+		velt++;
+	}
+
+	/*
 	for(volatile long int i = 0; i < 1000000000; i++){
 		fast_rand(&seed);
 		if((seed % 2) == 0){
@@ -13,4 +23,5 @@ int MCMPS() {
 	}
 
 	return sum;
+	*/
 }
