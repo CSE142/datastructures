6a9c2093c50d08dd6b5e34e0ce9bb9088627db2c
smreyes <smreyes@smreyes-6538.pods.smreyes.svc.cluster.local>
2021-05-16 10:57:07 -0700

    3

--- all_around.cpp
+++ all_around.cpp
@@ -4,7 +4,7 @@
 int all_around() {
 	uint64_t seed = 1;
 	uint64_t sum = 1;
-	for(volatile long int i = 0; i < 300000000; i++) {
+	for(volatile long int i = 0; i < 500000000; i++) {
 		fast_rand(&seed);
 		if((seed % 2) == 0) {
 			sum = sum + ((seed + i) ^ (seed & i) ^ (seed << i));
