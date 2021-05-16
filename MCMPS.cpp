cf459a9f92118132c13f709820cc3828a08dff36
smreyes <smreyes@smreyes-12614.pods.smreyes.svc.cluster.local>
2021-05-16 06:15:40 -0700

    3

--- MCMPS.cpp
+++ MCMPS.cpp
@@ -6,13 +6,15 @@ int MCMPS() {
 	uint64_t sum = 1;
 	//uint64_t arr[1024];
 	//fast_rand(&seed);
-	int n = 1000000000;
+	//int n = 1000000000;
 	//int block[n];
 
-	for(int i = 0; i < n/10; i++){
+	for(int i = 0; i < 1000000000; i++){
 		fast_rand(&seed);
 		//seed % n;
-		sum += (seed * sum);
+		if ((seed % 2) == 0) {
+			sum += (seed * sum);
+		}
 		
 	}
 
