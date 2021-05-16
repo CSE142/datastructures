9173cc3dcce261eb40ee4ee081260c0d4c33d820
smreyes <smreyes@smreyes-12163.pods.smreyes.svc.cluster.local>
2021-05-16 09:08:58 -0700

    d

--- MCMPS.cpp
+++ MCMPS.cpp
@@ -7,7 +7,7 @@ int MCMPS() {
 	register int sum = 1;
 	for(volatile long int i = 0; i < 1000000000; i++){
 		fast_rand(&seed);
-		for (long int j = seed; j < sizeof(seed); j++){
+		for (uint64_t int j = seed; j < sizeof(seed); j++){
 		//seed % n;
 		//if ((seed % 2) == 0) {
 			sum += (seed * sum);
