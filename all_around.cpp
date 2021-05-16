16a3e434b098f5fc455f367c42008faf0e1acc70
smreyes <smreyes@smreyes-12163.pods.smreyes.svc.cluster.local>
2021-05-16 05:54:36 -0700

    d

--- all_around.cpp
+++ all_around.cpp
@@ -13,7 +13,7 @@ int all_around() {
 	return sum;*/
 	uint64_t seed = 1;
 	uint64_t sum = 1;
-	uint64_t arr[1024];
+	//uint64_t arr[1024];
 	fast_rand(&seed);
 	for(long int i = 0; i < 1000000000; i++){
 			sum += i;
