1901918a884ac36f5b2cac841599dacb179e9440
smreyes <smreyes@smreyes-12163.pods.smreyes.svc.cluster.local>
2021-05-16 09:04:00 -0700

    d

--- MCMPS.cpp
+++ MCMPS.cpp
@@ -2,20 +2,21 @@
 #include <archlab.h>
 
 int MCMPS() {
-	/*
+	
 	uint64_t seed = 1;
-	uint64_t sum = 1;
-	for(int i = 0; i < 1000000000; i++){
+	register int sum = 1;
+	for(volatile long int i = 0; i < 1000000000; i++){
 		fast_rand(&seed);
+		for (int j = seed; j < seed * sizeof(sum); j++){
 		//seed % n;
-		if ((seed % 2) == 0) {
+		//if ((seed % 2) == 0) {
 			sum += (seed * sum);
 		}
 		
 	}
 
 	return sum;
-	*/
+
 
 	/*
 	uint64_t seed = 1;
@@ -28,6 +29,8 @@ int MCMPS() {
 	} 
 	return sum;
 	*/
+
+	/*
 	//int array[1024][1024];
 	uint64_t sum = 0;
 	uint64_t seed = 1;
@@ -43,5 +46,6 @@ int MCMPS() {
 		}
 	}
 	return sum;
+	*/
 
 }
