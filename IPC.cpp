ca36f060d06aabc83f07e2074f61bba558f2c629
smreyes <smreyes@smreyes-12163.pods.smreyes.svc.cluster.local>
2021-05-16 04:24:41 -0700

    d

--- IPC.cpp
+++ IPC.cpp
@@ -3,21 +3,23 @@
 
 
 int IPC() {
-	uint64_t s = 1; // you must initialize the seed to a non-zero
+	uint64_t seed = 1; // you must initialize the seed to a non-zero
 			// value or fast_rand will just return 0
 			// forever.
-
+	/*
 	for(int i= 0; i < 1; i++) {
 		// You should _definitely not_ print anything out in
 		// your actual code.  This is just to demonstrate that
 		// fast_rand() is working.
 		std::cout << fast_rand(&s) << "\n";
 	}
-
+*/
+	int sum = 1;
 
 	for(volatile long int i = 0; i < 1000000000; i++) {
-		 
+		fast_rand(&seed);
+		sum = sum + ((seed & i) ^ (seed	<< i));	
   	}
 
-	return 0;
+	return sum;
 }
