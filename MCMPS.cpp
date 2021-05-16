55d37241a63d9459a0a540549b65c286c2bd9b66
smreyes <smreyes@smreyes-7597.pods.smreyes.svc.cluster.local>
2021-05-16 05:30:00 -0700

    d

--- MCMPS.cpp
+++ MCMPS.cpp
@@ -2,13 +2,13 @@
 #include <archlab.h>
 
 int MCMPS() {
-	std::vector<uint32_t> v;
 	uint64_t seed = 1;
 	uint64_t sum = 1;
-	for(volatile long int i = 0; i < 1000000000; i++){
+	uint64_t arr[2048];
+	for(long int i = 0; i < 1000000000; i++){
 		fast_rand(&seed);
 		if((seed % 2) == 0){
-			sum+=sum;
+			sum += arr[seed];
 		}
 		
 	}
