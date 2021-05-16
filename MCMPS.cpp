3a52e82e1bbd0b3f5c0f65f212b6c0f53fe5be28
smreyes <smreyes@smreyes-7597.pods.smreyes.svc.cluster.local>
2021-05-16 05:06:27 -0700

    f

--- MCMPS.cpp
+++ MCMPS.cpp
@@ -3,19 +3,8 @@
 
 int MCMPS() {
 	std::vector<uint32_t> v;
-	//uint64_t seed = 1;
+	uint64_t seed = 1;
 	uint64_t sum = 1;
-	uint32_t velt = &v[0];
-	uint32_t vend = velt + v.size();
-	
-	while(velt < vend) {
-		sum += velt;
-		velt++;
-	}
-
-	return sum;
-
-	/*
 	for(volatile long int i = 0; i < 1000000000; i++){
 		fast_rand(&seed);
 		if((seed % 2) == 0){
@@ -25,5 +14,4 @@ int MCMPS() {
 	}
 
 	return sum;
-	*/
 }
