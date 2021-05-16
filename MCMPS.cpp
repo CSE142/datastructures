11f419c4d4a26f0052b07517406aad958581b2d7
smreyes <smreyes@smreyes-6538.pods.smreyes.svc.cluster.local>
2021-05-16 11:04:44 -0700

    3

--- MCMPS.cpp
+++ MCMPS.cpp
@@ -4,7 +4,7 @@
 int MCMPS() {
 
 //THIS has right runtime but not right values	
-	uint64_t seed = 1;
+	/*uint64_t seed = 1;
 	register int sum = 1;
 	for(volatile long int i = 0; i < 1000000000; i++){
 		fast_rand(&seed);
@@ -19,17 +19,19 @@ int MCMPS() {
 	}
 
 	return sum;
+	*/
 	
-	/*
+	
 	//uint64_t seed = 1;
+	int n = 10000000;
 	register int sum = 0;
-	//int i, j, k, a[1024][1024], b[1024][1024], c[1024][1024];
+	int i, j, k, a[n][n], b[n][n], c[n][n];
 	//int n = 1024;
-	for (long int x = 0; x < 100000000; x++) {
+	for (long int x = 0; x < n; x++) {
 		//fast_rand(&seed);
-		int arr[x];
-		for( long int y = 0; y < 1000000000 ; y++){
-			sum += arr[y];
+		//int arr[x];
+		for( long int y = 0; y < n ; y++){
+			sum += c[x][y];
 			if ((sum  & 2) == 0) {
 			sum = sum + ( sum ^ arr[y]);
 			}	
@@ -44,8 +46,8 @@ int MCMPS() {
 		}
 	}
 	}
-	return 0;
-*/
+	return sum;
+
 
 
 	/*
