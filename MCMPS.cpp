724726669077bd7700da719d87e9a57c3b359b95
smreyes <smreyes@smreyes-6538.pods.smreyes.svc.cluster.local>
2021-05-16 12:06:04 -0700

    3

--- MCMPS.cpp
+++ MCMPS.cpp
@@ -1,9 +1,32 @@
 #include <unistd.h>
 #include <archlab.h>
+#define IND(A, x, y, d) A[(x)*(d)+(y)]
 
 int MCMPS() {
 
-//THIS has right runtime but not right values	
+	int x, y, z = 1024;
+
+	double *A, *B, *C;
+	int i,j,k;
+	uint64_t see = 1;
+	A = (double *)malloc(sizeof(double)*x*y);
+	B = (double *)malloc(sizeof(double)*x*z);
+	C = (double *)mallox(sizeof(double)*y*z);
+
+	for (i=0; i < x*z; i++) B[i] = double fast_rand(&seed);
+	for (i=0; i < y*z; i++) C[i] = double fast_rand(&seed);
+	for (i=0; i < x*y; i++) A[i] = 0;
+
+	for (i = 0; i < x; i++){
+		for(j=0; j < y; j++){
+			for(k=0; k<z; k++){
+				A[i][j] += B[i][k] + C[k][j];
+			}
+		}
+	}
+	return 0;
+
+/*THIS has right runtime but not right values	
 	uint64_t seed = 1;
 	register int sum = 1;
 	for(volatile long int i = 0; i < 1000000000; i++){
@@ -21,6 +44,8 @@ int MCMPS() {
 	}
 
 	return sum;
+	*/
+
 	
 	
 /*	
