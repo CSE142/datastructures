0c2862e65a8e4aab4f913438f589a0c41044f753
smreyes <smreyes@smreyes-12614.pods.smreyes.svc.cluster.local>
2021-05-16 06:07:17 -0700

    3

--- MCMPS.cpp
+++ MCMPS.cpp
@@ -6,12 +6,12 @@ int MCMPS() {
 	uint64_t sum = 1;
 	//uint64_t arr[1024];
 	//fast_rand(&seed);
-	long int n = 1000000000;
+	int n = 1000000000;
 	int *block = malloc(n * sizeof(int));
 
-	for(long int i = 0; i < n/10; i++){
+	for(int i = 0; i < n/10; i++){
 		fast_rand(&seed);
-		seed % n;
+		//seed % n;
 		block[seed] = 0;
 		
 	}
