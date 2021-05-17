f898ce726a811c6f6b0a1769d69edc6be32de83a
smreyes <smreyes@smreyes-3501.pods.smreyes.svc.cluster.local>
2021-05-16 23:01:32 -0700

    d

--- MCMPS.cpp
+++ MCMPS.cpp
@@ -4,7 +4,7 @@
 
 int MCMPS() {
 
-
+/*
 	double *A; double *B; double *C;
 	int i,j,k;
 	uint64_t seed = 1;
@@ -31,7 +31,7 @@ int MCMPS() {
 		}
 	}
 	return 0;
-
+*/
 /*THIS has right runtime but not right values	
 	uint64_t seed = 1;
 	register int sum = 1;
@@ -50,37 +50,34 @@ int MCMPS() {
 	}
 
 	return sum;
-	*/
-
+*/	
 	
 	
-/*	
-	//uint64_t seed = 1;
+	uint64_t seed = 1;
 	long int n = 80000;
+	int x = 20000;
+	int y = 50000;
+	int z = 10000;
 	//register int sum = 0;
-	long int i, j, k, a[n][n], b[n][n], c[n][n];
+	long int i, j, k;
+       	int a[x][y], b[x][z], c[y][z];
+	for (i = 0; i < x*z; i++) b[i] = (int) fast_rand(&seed);
+	for (i = 0; i < y*z; i++) c[i] = (int) fast_rand(&seed);
+	for (i = 0; i < x*y; i++) a[i] = 0;
 	//int n = 1024;
-	//for (long int x = 0; x < n; x++) {
-		//fast_rand(&seed);
-		//int arr[x];
-		//for( long int y = 0; y < n ; y++){
-			//sum += c[x][y];
-			//if ((sum  & 2) == 0) {
-			//sum = sum + ( sum ^ c[x][y]);
-			//}	
-			for (i = 0; i < n ; i++){
-				for (j = 0; j < n; j++){
-					a[i][j] = a[i][j] + b[j][i];
-					for (k = 0; k < n; k++) {
-					c[i][j] = c[i][j] ^ a[i][k] * b[k][j];
+		for (i = 0; i < n ; i++){
+			for (j = 0; j < n; j++){
+				//a[i][j] = a[i][j] + b[j][i];
+				for (k = 0; k < n; k++) {
+				c[i][j] = c[i][j] ^ a[i][k] * b[k][j];
 
-				}
 			}
 		}
+	}
 //	}
 //	}
 	return 0;
-*/
+
 
 
 	/*
