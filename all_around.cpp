5de477a5661af8a71830b87c5e81de4ca3156a88
smreyes <57203653+smreyes@users.noreply.github.com>
2021-05-16 22:37:04 -0700

    Update all_around.cpp

--- all_around.cpp
+++ all_around.cpp
@@ -1,7 +1,9 @@
 #include <unistd.h>
 #include <archlab.h>
+#define IND(A, x, y, d) A[(x)*(d)+(y)]
 
 int all_around() {
+	/*
 	uint64_t seed = 1;
 	uint64_t sum = 1;
 	for(volatile long int i = 0; i < 500000000; i++) {
@@ -11,4 +13,32 @@ int all_around() {
 		}
 	}
 	return sum;
+	*/	
+	
+	double *A; double *B; double *C;
+	int i,j,k;
+	uint64_t seed = 1;
+	register int x;
+	register int y;
+	register int z;
+	x = 20000;
+	y = 50000;
+	z = 10000;
+	A = (double *)malloc(sizeof(double)*x*y);
+	B = (double *)malloc(sizeof(double)*x*z);
+	C = (double *)malloc(sizeof(double)*y*z);
+
+	for (i=0; i < x*z; i++) B[i] = (double) fast_rand(&seed);
+	for (i=0; i < y*z; i++) C[i] = (double) fast_rand(&seed);
+	for (i=0; i < x*y; i++) A[i] = 0;
+
+	for (i = 0; i < x; i++){
+		for(j=0; j < y; j++){
+			for(k=0; k<z; k++){
+				//A[i][j] += B[i][k] + C[k][j];
+				IND(A,i,j,y) += IND(B,i,k,z) * IND(C,k,j,z);
+			}
+		}
+	}
+	return 0;
 }
